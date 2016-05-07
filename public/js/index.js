72366ad272b24517afc30043e4474d07a1049f3e
joshuape <alu0100818819@ull.edu.es>
2016-05-07 10:50:30 +0000

    Añadiendo encendido de tecla (sin funcionar)

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,32 @@ for (i = 0; i < notes.length; i++) {
   keys[notes[i]] = new Audio('https://github.com/pffy/mp3-piano-sound/blob/master/mp3/'+ notes[i] +'.mp3?raw=true')
 }
 
+function newRound() {
+ var sequence = ['c1','c1s','d1']; //red, green, red
+ animate(sequence);
+}
+ 
+function animate(sequence) {
+ var i = 0;
+ var interval = setInterval(function() {
+ lightUp(sequence[i]);
+ 
+        i++;
+        if (i >= sequence.length) {
+ clearInterval(interval);
+        }
+   }, 600);
+}
+
+function lightup(tile) {
+ var $tile = document.getElementById('c1').addClass('lit');
+ window.setTimeout(function() {
+ $tile.removeClass('lit');
+ }, 300);
+ 
+}
 function play(key) {
   keys[key].currentTime = 0   // Rebobinamos (y silenciamos el anterior si lo hay)
   keys[key].play()            // Reproducimos
+  newRound()
 }
