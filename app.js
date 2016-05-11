b4ed0471f40b6f8955289e895f65fb7dfdbe5d98
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2016-05-11 09:36:12 +0000

    Añadiendo ruta para guardar el usuario y su puntuación

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 
+const usu = require('./models/schema_puntuaciones');
+
 app.set('port', (process.env.PORT || 8080));
 
 app.set('views',path.join(__dirname, 'views'));
@@ -15,6 +17,24 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req,res) => {
   res.render('index');
+});
+
+app.get('/puntuaciones/:nombre', (res,req) => {
+  
+  let usr = new usu({
+    "name": req.nombre,
+    "punt": req.query.puntu
+  }); 
+    
+  usr.save(function (err) {
+      
+    if(err) {
+      console.log(`Error:\n ${err}`);
+      return err
+    }
+    console.log(`Guardado: ${usr}`);  
+  });
+    
 });
 
 app.listen(app.get('port'), () => {
