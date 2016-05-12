41878975e56337799df1b7df84350a21b38eec1e
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2016-05-12 09:54:23 +0000

    Añadiendo más botones

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,17 @@
 var notes = ['c1', 'c1s', 'd1', 'd1s', 'e1', 'f1', 'f1s', 'g1', 'g1s', 'a1', 'a1s', 'b1', 'c2']
+var cumple = ['c1','c1','d1','c1','f1','e1','c1','c1','d1','c1','g1','f1']; 
+var can1 = ['d1','c1'];
+
 var keys = {}
 var piano = document.getElementById('piano')
 
 // Preload audio
 for (i = 0; i < notes.length; i++) {
-  //piano.innerHTML += '<div id="'+notes[i]+'" class="'+ (notes[i][2] == 's'?'b':'') +'key" onClick="play(this.id)"></div>';
-  keys[notes[i]] = new Audio('https://github.com/pffy/mp3-piano-sound/blob/master/mp3/'+ notes[i] +'.mp3?raw=true')
+ keys[notes[i]] = new Audio('https://github.com/pffy/mp3-piano-sound/blob/master/mp3/'+ notes[i] +'.mp3?raw=true')
 }
 
-function newRound() {
- var sequence = ['c1','c1','d1','c1','f1','e1','c1','c1','d1','c1','g1','f1']; //red, green, red
+function newRound(sequence) {
+ console.log(sequence);
  animate(sequence);
 }
 
@@ -38,16 +40,18 @@ function lightUp(tile) {
 function play(key) {
   keys[key].currentTime = 0   // Rebobinamos (y silenciamos el anterior si lo hay)
   keys[key].play()            // Reproducimos
- // newRound()
 }
 
  $("#guardar").click(() => {
   var p = 0;
         console.log($("#nombreusu").val());
-        //app.get('/puntuaciones/:nombre', (res,req)
-        //$.get("/ficheros/" + $("#nombrefich").val(), {
         $.get("/puntuaciones/" + $("#nombreusu").val(), {
          "punt": p
          
       });
     });
+    
+$("#canciones").click(function(e) {
+  console.log(e.target.id);
+  newRound(e.target.id);
+});
