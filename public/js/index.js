88c1de7c8768ffc25997ae858887bc09ed7db1b8
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2016-05-18 09:57:27 +0000

    Función de puntuación mejorada

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@ var notes = ['c1', 'c1s', 'd1', 'd1s', 'e1', 'f1', 'f1s', 'g1', 'g1s', 'a1', 'a1
 var cumple = ['c1','c1','d1','c1','f1','e1','c1','c1','d1','c1','g1','f1']; 
 var can1 = ['d1','c1'];
 var p = 0;
+var tocado = 0;
 
 var keys = {};
 
@@ -15,12 +16,11 @@ function newRound(sequence) {
 }
 
 function puntuacion(key){
-  for(var i = 0; i<cumple.length; i++)
-  {
-    if(key === cumple[i]){
-      return (p + 1);
+  console.log(key);
+  if(key === cumple[tocado]){
+      tocado++;
+      p++;
     }
-  }
 }
 
 function animate(sequence) {
@@ -31,7 +31,7 @@ function animate(sequence) {
     if (i >= sequence.length) {
      clearInterval(interval);
     }
-  }, 1000);
+  }, 1200);
   return interval;
 }
 
@@ -39,14 +39,13 @@ function lightUp(tile) {
   var aux = $('#' + tile).addClass('lit');
   window.setTimeout(function() {
    aux.removeClass('lit');
-  }, 400);
+  }, 600);
 }
 
 function play(key) {
   keys[key].currentTime = 0;   // Rebobinamos (y silenciamos el anterior si lo hay)
   keys[key].play();// Reproducimos
-  if(puntuacion(key) !== undefined)
-    p = puntuacion(key);
+  puntuacion(key);
   console.log(p);
 }
 
